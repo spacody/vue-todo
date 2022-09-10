de2012ceee01a113fde677e0aa6e3c5740a0cbd2
spacody <tefoh2@gmail.com>
2022-09-10 17:38:03 +0430

    refactor App component to typescript

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
-<script setup>
+<script setup lang="ts">
 import { reactive, onMounted, watch } from 'vue';
+
+import type TodoType from './types/TodoType'
 
 import TodoHeader from './components/TodoHeader.vue';
 import Todos from './components/Todos.vue';
 
-const todos = reactive([]);
+const todos: TodoType[] = reactive([]);
 
-watch(todos, (newValue) => {
+watch(todos, (newValue: TodoType[]) => {
   localStorage.setItem('todos', JSON.stringify(newValue));
 });
 
 onMounted(() => {
-  const oldTodos = JSON.parse(localStorage.getItem('todos')) || [];
+  const oldTodos = JSON.parse(localStorage.getItem('todos') || '') || [];
   todos.push(...oldTodos);
 });
 
-const addTodo = (todo) => {
+const addTodo = (todo: TodoType) => {
   todos.push(todo);
 }
 
-const checkTodo = (todo) => {
-  const foundTodo = todos.find(item => item === todo);
+const checkTodo = (todo: TodoType) => {
+  const foundTodo: TodoType|undefined = todos.find(item => item === todo);
+
+  if (! foundTodo) {
+    return;
+  }
 
   foundTodo.checked = !foundTodo.checked;
 }
 
-const deleteTodo = (index) => {
+const deleteTodo = (index: number) => {
   todos.splice(index, 1);
 }
 </script>
