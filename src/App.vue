8b017ff4216007c494d3f4313c4034f20cc36e43
spacody <tefoh2@gmail.com>
2022-09-10 15:13:59 +0430

    persist todos data to localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { reactive, onMounted } from 'vue';
 
 import TodoHeader from './components/TodoHeader.vue';
 import Todos from './components/Todos.vue';
 
 const todos = reactive([]);
 
+onMounted(() => {
+  const oldTodos = JSON.parse(localStorage.getItem('todos')) || [];
+  todos.push(...oldTodos);
+});
+
 const addTodo = (todo) => {
   todos.push(todo);
+
+  updateStorageTodos();
 }
 
 const checkTodo = (todo) => {
   const foundTodo = todos.find(item => item === todo);
 
   foundTodo.checked = !foundTodo.checked;
+  updateStorageTodos();
 }
 
 const deleteTodo = (index) => {
   todos.splice(index, 1);
+}
+
+const updateStorageTodos = () => {
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
 </script>
 
