74e772e12a4dc2ad3d246f78f077259427d810c5
spacody <tefoh2@gmail.com>
2022-09-10 15:20:27 +0430

    persist todos to localStorage on everychange with watch

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
-import { reactive, onMounted } from 'vue';
+import { reactive, onMounted, watch } from 'vue';
 
 import TodoHeader from './components/TodoHeader.vue';
 import Todos from './components/Todos.vue';
 
 const todos = reactive([]);
+
+watch(todos, () => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+});
 
 onMounted(() => {
   const oldTodos = JSON.parse(localStorage.getItem('todos')) || [];
@@ -13,23 +17,16 @@ onMounted(() => {
 
 const addTodo = (todo) => {
   todos.push(todo);
-
-  updateStorageTodos();
 }
 
 const checkTodo = (todo) => {
   const foundTodo = todos.find(item => item === todo);
 
   foundTodo.checked = !foundTodo.checked;
-  updateStorageTodos();
 }
 
 const deleteTodo = (index) => {
   todos.splice(index, 1);
-}
-
-const updateStorageTodos = () => {
-  localStorage.setItem('todos', JSON.stringify(todos));
 }
 </script>
 
